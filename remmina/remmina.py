8b83f2cfa5ca8a78f711f68499467a3855441cb3
hugosenari <hugosenari@gmail.com>
2016-10-15 17:32:23 -0200

    convert remmina plugin to new format

--- remmina/remmina.py
+++ remmina/remmina.py
@@ -17,45 +17,47 @@ import fnmatch
 import os
 import re
 
+default_cfg_dir = os.path.join(
+    os.getenv('USERPROFILE') or os.getenv('HOME'),
+    '.local', 'share', 'remmina')
 
 __kupfer_settings__ = PluginSettings( 
         {
-                "key" : "remmina_config_folder",
+                "key" : "remmina_hosts_folder",
                 "label": _("Remina config folder"),
                 "type": str,
-                "value": os.path.join(
-                                (os.getenv('USERPROFILE') or os.getenv('HOME')),
-                                '.remmina'),
+                "value": default_cfg_dir,
         }
 )
 
 class ReminnaHostServiceLeaf(HostServiceLeaf):
     def __init__(self, item):
-        HostServiceLeaf.__init__(self, item.get('name'),
-                     item.get('server')
-                            or item.get('ssh_server'),
-                     item.get('protocol'),
-                     item.get('description'),
-                     item.get('port'),
-                     item.get('username')
-                            or item.get('ssh_username'),
-                     item.get('password')
-                            or item.get('ssh_password'),
-                     item)
+        HostServiceLeaf.__init__(
+             self, item.get('name'),
+             item.get('server') or item.get('ssh_server'),
+             item.get('protocol'),
+             item.get('description'),
+             item.get('port'),
+             item.get('username') or item.get('ssh_username'),
+             item.get('password') or item.get('ssh_password'),
+             item)
+
     def get_icon_name(self):
         return "network-server"
 
-class ReminaHostServiceSource(AppLeafContentMixin, ToplevelGroupingSource,
-                  FilesystemWatchMixin):
+
+class ReminaHostServiceSource(AppLeafContentMixin,
+                ToplevelGroupingSource, FilesystemWatchMixin):
     '''Remmina leaf factory'''
     appleaf_content_id = 'remmina'
+    
     def __init__(self, name=_("Remmina Hosts")):
         ToplevelGroupingSource.__init__(self, name, _("Remmina"))
-        self._remina_home = __kupfer_settings__["remmina_config_folder"]
 
     def initialize(self):
         ToplevelGroupingSource.initialize(self)
-        self.monitor_token = self.monitor_directories(self._remina_home)    
+        cfg_dir = __kupfer_settings__["remmina_hosts_folder"]
+        self.monitor_token = self.monitor_directories(cfg_dir)    
 
     def provides(self):
         yield ReminaHostServiceSource
@@ -67,22 +69,12 @@ class ReminaHostServiceSource(AppLeafContentMixin, ToplevelGroupingSource,
         return "remmina"
 
     def get_items(self):
-        try:
-            for file in os.listdir(self._remina_home):
-                try:
-                    uri = os.path.join(self._remina_home, file)
-                    if fnmatch.fnmatch(file, '*.remmina'):
-                        yield self.parse_file(uri)
-                except Exception, exc:
-                    pretty.print_error(
-                        __name__,
-                        type(exc).__name__,
-                        exc)
-        except Exception, exc:
-            pretty.print_error(
-                __name__,
-                type(exc).__name__,
-                exc)
+        cfg_dir = __kupfer_settings__["remmina_hosts_folder"]
+        cfg_files = os.listdir(cfg_dir)
+        for cfg_file in cfg_files:
+            uri = os.path.join(cfg_dir, cfg_file)
+            if fnmatch.fnmatch(cfg_file, '*.remmina'):
+                yield self.parse_file(uri)
 
     def parse_file(self, uri):
         return ReminnaHostServiceLeaf(
@@ -91,15 +83,14 @@ class ReminaHostServiceSource(AppLeafContentMixin, ToplevelGroupingSource,
     @staticmethod
     def _parse_file(uri):
         result = {'path': uri}
-        file = open(uri)
-        reg = re.compile("([^=]+)=(.+)")
-        for line in file.readlines():
-            match = reg.match(line)
-            if match:
-                key, value = match.groups()
-                key = key.strip()
-                result[key] = value.strip()
-        file.close()
+        with open(uri) as cfg_file:
+            reg = re.compile("\s*([^=]+)\s*=\s*(.+)\s*")
+            for line in cfg_file.readlines():
+                match = reg.match(line)
+                if match:
+                    key, value = match.groups()
+                    result[key] = value.strip()
+
         result['description'] = "%s : %s : %s : %s" % (
             result.get('group'),
             result.get('server') or result.get('ssh_server'),
@@ -107,6 +98,7 @@ class ReminaHostServiceSource(AppLeafContentMixin, ToplevelGroupingSource,
             result.get('name'))
         pretty.print_debug(__name__, result['description'])
         return result
+
 
 class RemminaConnect(Action):
     '''Used to launch remmina connecting to the specified config file.'''
@@ -120,15 +112,12 @@ class RemminaConnect(Action):
         return "remmina"
 
     def item_types(self):
-        yield HostLeaf
-        yield HostServiceLeaf
         yield ReminnaHostServiceLeaf
 
     def valid_for_item(self, leaf):
         """Whether action can be used with exactly @item"""
         return leaf.check_key('path')\
-            or leaf.check_key('TSCLIENT_SESSION')\
-            #or isinstance(leaf, HostServiceLeaf)
+            or leaf.check_key('TSCLIENT_SESSION')
 
     def activate(self, leaf):
         obj = leaf.object
