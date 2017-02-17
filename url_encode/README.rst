b877de7ae1984b13bc61da54d75e5b695006ef62
Hugo Ribeiro <hugo.ribeiro@net.com.br>
2017-02-17 17:00:10 -0200

    novo plugin para encode/decode de url

---
+++ url_encode/README.rst
@@ -0,0 +1,44 @@
+===============================
+URL Encode
+===============================
+
+
+.. image:: https://img.shields.io/pypi/v/kupfer_plugin_url_encode.svg
+        :target: https://pypi.python.org/pypi/kupfer_plugin_url_encode
+
+.. image:: https://img.shields.io/travis/hugosenari/kupfer_plugin_url_encode.svg
+        :target: https://travis-ci.org/hugosenari/kupfer_plugin_url_encode
+
+.. image:: https://readthedocs.org/projects/kupfer_plugin_url-encode/badge/?version=latest
+        :target: https://kupfer_plugin_url-encode.readthedocs.io/en/latest/?badge=latest
+        :alt: Documentation Status
+
+
+
+Decode and encode actions for kupfer
+
+Require Kupfer http://engla.github.io/kupfer/
+
+
+* Free software: MIT license
+
+
+Features
+--------
+
+* TODO
+
+Install
+-------
+
+pip install kupfer_plugin_url_encode
+
+
+Credits
+-------
+
+This package was created with Cookiecutter_ and the `cookiecutter-kupfer-plugin-package`_ project template.
+
+.. _Cookiecutter: https://github.com/audreyr/cookiecutter
+.. _`cookiecutter-kupfer-plugin-package`: https://github.com/hugosenari/cookiecutter-kupfer-plugin-package
+
---
+++ url_encode/url_encode.py
@@ -0,0 +1,47 @@
+__kupfer_name__ = _("URL Encode")
+__version__ = "0.1.0"
+__author__ = "Hugo Sena Ribeiro <hugosenari@gmail.com>"
+__description__ = _("""Decode and encode actions for kupfer""")
+__kupfer_actions__ = ("URLEncode", "URLDecode")
+
+
+import urllib
+from kupfer.objects import Action, UrlLeaf, TextLeaf
+
+
+class URLEncode(Action):
+    def __init__(self):
+        Action.__init__(self, name=_("URL Encode"))
+    
+    def activate(self, obj):
+        result = ''
+        try:
+            result = urllib.quote(obj.object.encode('utf8'))
+        except AttributeError:
+            result = urllib.parse.quote(obj.object.encode('utf8'))
+        return TextLeaf(result)
+    
+    def item_types(self):
+        return UrlLeaf, TextLeaf
+    
+    def has_result(self):
+        return True
+
+
+class URLDecode(Action):
+    def __init__(self):
+        Action.__init__(self, name=_("URL Decode"))
+    
+    def activate(self, obj):
+        result = ''
+        try:
+            result = urllib.unquote(obj.object)
+        except AttributeError:
+            result = urllib.parse.unquote(obj.object)
+        return TextLeaf(result)
+    
+    def item_types(self):
+        return UrlLeaf, TextLeaf
+    
+    def has_result(self):
+        return True
