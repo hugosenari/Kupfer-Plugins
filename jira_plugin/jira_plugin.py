627156f75c393bab11af5ee626ad2a931fc75177
Hugo Ribeiro <hugo.ribeiro@net.com.br>
2017-02-22 16:43:53 -0300

    Bump version: 0.1.0 → 0.1.1

--- jira_plugin/jira_plugin.py
+++ jira_plugin/jira_plugin.py
@@ -1,5 +1,5 @@
 __kupfer_name__ = _('Jira')
-__version__ = _('0.1.0')
+__version__ = _('__version__ = "0.1.1"')
 __author__ = _('Hugo Sena Ribeiro <hugosenari@gmail.com>')
 __description__ = _('''Kupfer plugin to control Jira''')
 
