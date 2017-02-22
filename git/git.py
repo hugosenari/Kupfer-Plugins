5bb82a8ad5786cdf0ad332688485d2f3f3cf1c4c
Hugo Ribeiro <hugo.ribeiro@net.com.br>
2017-02-22 15:12:32 -0300

    Bump version: 0.1.0 → 0.1.1

--- git/git.py
+++ git/git.py
@@ -1,5 +1,5 @@
 __kupfer_name__ = _("Git")
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "hugosenari <hugosenari@gmail.com>"
 __description__ = _("""Git plugin for kupfer""")
 __kupfer_actions__ = ('GitActions', 'GitkAction', 'ChangeBranchAction',
