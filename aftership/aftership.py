4fbb273576fdcea012eb51dfaadf03e9e0941914
hugosenari <hugosenari@gmail.com>
2017-04-20 03:40:49 -0200

    add ViewPage to AfterShip plugin

--- aftership/aftership.py
+++ aftership/aftership.py
@@ -1,21 +1,21 @@
 ## read plugin api:
 ## https://kupferlauncher.github.io/Documentation/PluginAPI.html
-_ = lambda x: x
 __kupfer_name__ = _('AfterShip')
 __version__ = '0.1.0'
 __author__ = 'Hugo Sena Ribeiro <hugosenari@gmail.com>'
 __description__ = '''Uses AfterShip for package tracking'''
-__kupfer_actions__ = ('PackageStatus',)
-__kupfer_sources__ = ('AfterShipCurriers',)
+__kupfer_actions__ = ('PackageStatus', 'ViewPage')
+__kupfer_sources__ = ('Curriers',)
   
 from json import loads
 from urllib import request, error
 from kupfer.plugin_support import PluginSettings
 from kupfer.objects import Action, Source, TextLeaf
+from kupfer.utils import show_url
 
 __kupfer_settings__ = PluginSettings( 
     {
-        'key' : 'aftership_key',
+        'key' : 'api_key',
         'label': _('AfterShip Key'),
         'type': str,
         'value': '92139263-a1d7-40a4-847b-d82f536b51ea',
@@ -24,38 +24,55 @@ __kupfer_settings__ = PluginSettings(
 
 CURRIERS_ENDPOINT = 'https://api.aftership.com/v4/couriers/all'
 ENDPOINT = 'https://api.aftership.com/v4/last_checkpoint/{}/{}'
+LINK = 'https://track.aftership.com/{}/{}'
 
 
-def aftership_request(url):
+def api_request(url):
     req = request.Request(
         url,
         headers={
             'Content-Type': 'application/json',
-            'aftership-api-key': __kupfer_settings__['aftership_key']
+            'aftership-api-key': __kupfer_settings__['api_key']
         }
     )
     with request.urlopen(req) as service:
         return loads(service.read())
 
 
-class AfterShipCurrier(TextLeaf):
+class Currier(TextLeaf):
     def __init__(self, obj):
         TextLeaf.__init__(self, obj['slug'], obj['name'])
 
 
-class AfterShipCurriers(Source):
+class PackageStatusLeaf(TextLeaf):
+    def __init__(self, obj, code, currier):
+        self.obj = obj
+        self.code = code
+        self.currier = currier
+        txt = self.obj.get('meta', {}).get('message', '') 
+        checkpoint = self.obj.get('data', {}).get('checkpoint', {})
+        if checkpoint:
+            if checkpoint.get('message'):
+                fmt = '{message} {country_name} {checkpoint_time}'
+                txt = fmt.format(**checkpoint)
+            else:
+                txt = 'Empty status info'
+        TextLeaf.__init__(self, txt)
+
+
+class Curriers(Source):
     def __init__(self):
         Source.__init__(self, 'AfterShip Curriers')
     
     def produces(self):
-        yield AfterShipCurrier
+        yield Currier
     
     def get_items(self):
-        json = aftership_request(CURRIERS_ENDPOINT)
+        json = api_request(CURRIERS_ENDPOINT)
         curriers = json.get('data', {}).get('couriers', ())
         for currier in curriers:
             if not currier['required_fields']: 
-                yield AfterShipCurrier(currier)
+                yield Currier(currier)
 
 
 class PackageStatus(Action):
@@ -67,18 +84,10 @@ class PackageStatus(Action):
         url = ENDPOINT.format(currier_leaf.object, leaf.object)
         json = {}
         try:
-            json = aftership_request(url)
+            json = api_request(url)
         except error.HTTPError as err:
             json = {'meta':{'message': err.reason, 'code': err.code}}
-        txt = json.get('meta', {}).get('message', '') 
-        checkpoint = json.get('data', {}).get('checkpoint', {})
-        if checkpoint:
-            if checkpoint.get('message'):
-                fmt = '{message} {country_name} {checkpoint_time}'
-                txt = fmt.format(**checkpoint)
-            else:
-                txt = 'Empty status info'
-        return TextLeaf(leaf.object, txt)
+        return PackageStatusLeaf(json, leaf.object, currier_leaf.object)
   
     def has_result(self):
         return True
@@ -90,9 +99,17 @@ class PackageStatus(Action):
         return True
 
     def object_types(self, for_item=None):
-        yield AfterShipCurrier
+        yield Currier
 
 
-if __name__ == '__main__':
-    json = aftership_request(CURRIERS_ENDPOINT)
-    print(json)
+class ViewPage(Action):
+    
+    def __init__(self):
+        Action.__init__(self, name=_('View at AfterShip'))
+  
+    def activate(self, leaf):
+        url = LINK.format(leaf.currier, leaf.code)
+        show_url(url)
+
+    def item_types(self):
+        yield PackageStatusLeaf
